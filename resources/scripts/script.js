fb1a3a217179fffcbdf6a7b55e5331f550564079
Emiliano Berestovoy <emiliano.berestovoy@accenture.com>
2024-03-05 18:40:02 -0300

    Jugando con estilos

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -2,9 +2,22 @@
 //     console.log(res.data);
 // });
 
+
+const background = [
+    { img: 'bg_01.webp', el: 'moon' },
+    { img: 'bg_02.webp', el: 'sun' },
+    { img: 'bg_03.webp', el: 'moon' },
+    { img: 'bg_04.webp', el: 'moon' },
+    { img: 'bg_06.webp', el: 'sunRed' },
+    { img: 'bg_07.webp', el: 'sun' },
+    { img: 'bg_08.webp', el: 'sun' },
+    { img: 'bg_09.webp', el: 'sun' },
+    { img: 'bg_10.webp', el: 'sun' },
+];
+
 const setBackGround = () => {
-    const background = [{ img: 'bg_01.webp', el: 'moon' }, { img: 'bg_02.webp', el: 'sun' }];
-    const index = Date.now() % 2;
+    // const background = [{ img: 'bg_01.webp', el: 'moon' }, { img: 'bg_02.webp', el: 'sun' }];
+    const index = Date.now() % background.length;
     const main = document.querySelector('.main');
     const sunMoonEl = document.querySelector(`.${background[index].el}`);
     main.style.backgroundImage = `url('./resources/images/${background[index].img}')`;
